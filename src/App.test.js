77a35b727154792296dcf94b55a704e159f0fc31
Ubuntu <ubuntu@ip-172-31-94-76.ec2.internal>
2022-04-17 20:09:18 +0000

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,13 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText("Hola desde aws Jeru :D");
+  expect(linkElement).toBeInTheDocument();
+});
+
+
+test('renders tqm muchito', () => {
+  render(<App />);
+  const linkElement = screen.getByText("tqm muchito");
   expect(linkElement).toBeInTheDocument();
 });
